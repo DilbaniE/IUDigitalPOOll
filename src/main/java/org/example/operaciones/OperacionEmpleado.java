15fa63f8210bb00e0245e58d2183cedf27f31c1b
DilbaniE <claudiaenriquez241@gmail.com>
2026-02-25 20:44:15 -0500

    structure

---
+++ src/main/java/org/example/operaciones/OperacionEmpleado.java
@@ -0,0 +1,4 @@
+package org.example.operaciones;
+
+public class OperacionEmpleado implements IOperacionEmpresa {
+}
---
+++ src/main/java/org/example/operaciones/OperacionEmpresa.java
@@ -0,0 +1,4 @@
+package org.example.operaciones;
+
+public class OperacionEmpresa implements IOperacionEmpresa {
+}
